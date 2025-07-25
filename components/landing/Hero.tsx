15d315687a508b7055fe71377d71a5794b9df658
Sean Lai <seanlai221@gmail.com>
2025-07-24 20:57:59 -0400

    Simplified loading

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -4,7 +4,7 @@ import { searching } from '@/lib/consts'
 import { MaskText } from '../ui/inlinemask/inlineMask'
 import { AsciiNoiseBackground } from './asciihero'
 import Anim_Button from '../ui/animated_a'
-import TextMask from '../ui/inlinemask/inlineCompmask'
+import { ComponentMask } from '../ui/inlinemask/inlineCompmask'
 const HeroSection = () => {
   return (
     <section className="min-h-[50vh] lg:min-h-[80vh] w-full text-black flex flex-col justify-center md:px-6 bg-transparent scroll-hidden">
@@ -32,9 +32,9 @@ function LandingInformation() {
         </h1>
 
         <div className="mt-2 text-left text-sm sm:text-base md:text-lg max-w-sm sm:max-w-md md:max-w-xl text-beige">
-          <TextMask
+          <MaskText
             phrases={["Building Smarter Research Infra & Data Tools", "Trekking into the complexities of our world one data point at a time"]}
-            className="text-sm sm:text-base md:text-lg text-beige"
+            style="text-sm sm:text-base md:text-lg text-beige"
             customDelay={0.7}
             duration={0.9}
           />
@@ -43,67 +43,62 @@ function LandingInformation() {
           <Anim_Button content="Resume" href="/Sean%20Lai%20Resume.pdf" funcAct={() => {}} style='mt-5 min-w-10 max-w-[10rem]'/>
       </div>
     <div className="lg:bg-transparent lg:backdrop-blur-md p-2 py-4 sm:p-4 lg:p-10 w-full max-w-full md:w-1/2 h-full flex items-center justify-start md:items-start md:justify-start text-xs sm:text-base lg:text-base lg:leading-7 text-beige overflow-x-hidden">
-      <TextMask
-        phrases={[
-          <>
-            Building data infra at{' '}
-            <a
-              href="https://www.boostb2b.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline transition-colors"
-            >
-              Boost Payment Solutions
-            </a>.
-          </>,
-          <>
-            VP @{' '}
-            <a
-              href="https://www.techatnyu.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline transition-colors"
-            >
-              Tech@NYU
-            </a>{' '}
-            – run hiring hackathons and curated tech events.
-          </>,
-          <>
-            Research Eng Lead @{' '}
-            <a
-              href="https://vip.hsrn.nyu.edu"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline transition-colors"
-            >
-              NYU HSRN Lab
-            </a>{' '}
-            – building low latency research workflows.
-          </>,
-          <>
-            Prev: VLM Research @{' '}
-            <a
-              href="https://ai4ce.github.io/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline transition-colors"
-            >
-              NYU AI4CE Lab
-            </a>{' '}
-            & Research Assistant @{' '}
-            <a
-              href="https://engineering.nyu.edu/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-blue-500 hover:underline transition-colors"
-            >
-              NYU Sustainablity Intiative
-            </a>.
-          </>,
-        ]}
-        className="p-2 text-left leading-relaxed"
-        newLine
-      />
+      <p>
+        Building data infra at{' '}
+        <a
+          href="https://www.boostb2b.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline transition-colors"
+        >
+          Boost Payment Solutions
+        </a>. 
+        <br />
+        <br />
+        VP @{' '}
+        <a
+          href="https://www.techatnyu.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline transition-colors"
+        >
+          Tech@NYU
+        </a>{' '}
+        – run hiring hackathons and curated tech events.
+        <br />
+        <br />
+        Research Eng Lead @{' '}
+        <a
+          href="https://vip.hsrn.nyu.edu"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline transition-colors"
+        >
+          NYU HSRN Lab
+        </a>{' '}
+        – building low latency research workflows. 
+        <br />
+        <br />
+        Prev: VLM Research @{' '}
+        <a
+          href="https://ai4ce.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline transition-colors"
+        >
+          NYU AI4CE Lab
+        </a>{' '}
+        & Research Assistant @{' '}
+        <a
+          href="https://engineering.nyu.edu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline transition-colors"
+        >
+          NYU Sustainability Initiative
+        </a>.
+      </p>
+        
 
     </div>
   </div>
--- components/ui/inlinemask/inlineCompmask.tsx
+++ components/ui/inlinemask/inlineCompmask.tsx
@@ -1,62 +1,30 @@
 'use client'
-import React, { ReactNode, FC } from 'react'
-import { motion } from 'framer-motion'
-import { useInView } from 'react-intersection-observer'
 import styles from './style.module.css'
+import { motion } from 'framer-motion';
+import { ReactNode } from 'react';
+import { useInView } from 'react-intersection-observer';
+export function ComponentMask({phrases, style, customDelay  = 0, duration = 0.75, newLine=false}:{phrases: ReactNode[], style?:string, customDelay?: number, duration?: number, newLine?: boolean}) {
 
-interface TextMaskProps {
-  phrases: ReactNode[]
-  className?: string
-  customDelay?: number
-  duration?: number
-  newLine?: boolean
+    const animation = {
+      initial: {y: "100%"},
+      enter: (i: number) => ({y: "0", transition: {duration: duration, ease: [0.33, 1, 0.68, 1],  delay: 0.075 * i  + customDelay}})
+    }
+  
+    const { ref, inView } = useInView({
+      threshold: 0.75,
+      triggerOnce: true
+    });
+  
+    return(
+      <div ref={ref} className={`${style} h-fit`}>
+        {
+          phrases.map( (phrase, index) => {
+            return <div key={index} className={styles.lineMask}>
+              <motion.p custom={index} variants={animation} initial="initial" animate={inView ? "enter" : ""}>{phrase}</motion.p>
+              {newLine && <><br /><br /></>}
+            </div>
+          })
+        }
+      </div>
+    )
 }
-
-export const TextMask: FC<TextMaskProps> = ({
-  phrases,
-  className = '',
-  customDelay = 0,
-  duration = 0.75,
-  newLine = false,
-}) => {
-  const animation = {
-    initial: { y: '100%' },
-    enter: (i: number) => ({
-      y: '0',
-      transition: {
-        delay: customDelay + 0.075 * i,
-        duration,
-        ease: [0.33, 1, 0.68, 1],
-      },
-    }),
-  }
-
-  const { ref, inView } = useInView({
-    threshold: 0.75,
-    triggerOnce: true,
-  })
-
-  return (
-    <div ref={ref} className={`${className} h-fit`}>
-      {phrases.map((phrase, i) => (
-        <div key={i} className={styles.lineMask}>
-          <motion.p
-            custom={i}
-            variants={animation}
-            initial="initial"
-            animate={inView ? 'enter' : undefined}
-          >
-            {phrase}
-          </motion.p>
-          {newLine && i < phrases.length - 1 && (
-            <>
-              <br />
-            </>
-          )}
-        </div>
-      ))}
-    </div>
-  )
-}
-
-export default TextMask
